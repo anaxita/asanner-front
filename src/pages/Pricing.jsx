df32169c20eb91eb28ceb369d8ad1f31f619795e
Alexandr Kostiuchenko <a.kastsiuchenka@softswiss.com>
2023-09-24 17:36:19 +0400

    get sub plans from api

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,41 +1,51 @@
+import { useEffect, useState } from 'react';
 import { Button, Card, ListGroup } from 'react-bootstrap';
+
+import { makeHttpRequest } from '../api/makeHttpRequest';
 
 export const Pricing = () => {
   const { subscription } = JSON.parse(localStorage.getItem('profile'));
+
+  const { plans, setPlans } = useState([]);
+
+  useEffect(() => {
+    makeHttpRequest('GET', '/plans').then((response) => {
+      const { data, err } = response;
+
+      if (err) {
+        console.log('/plans error', err);
+        // TODO handle error
+      }
+
+      if (data) {
+        setPlans(data);
+      }
+    });
+  }, []);
+
+  const planCards = plans.map((plan) => {
+    return (
+      <Card className="text-center" style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{plan.name}</Card.Title>
+          <ListGroup className="mb-2">
+            {plan.oppotunities.map((v) => {
+              return <ListGroup.Item>{v}</ListGroup.Item>;
+            })}
+          </ListGroup>
+          <Button disabled={subscription?.subscription_id === plan.id} variant="secondary">
+            {subscription?.subscription_id === plan.id ? 'Текущий' : 'Перейти'}
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  });
 
   return (
     <>
       <div className="d-flex align-items-center flex-column">
         <h1 className=" title">Pricing</h1>
-        <div className="d-flex gap-3">
-          <Card className="text-center" style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>Базовый</Card.Title>
-              <ListGroup className="mb-2">
-                <ListGroup.Item as="li">Преимущество 1</ListGroup.Item>
-                <ListGroup.Item as="li">Преимущество 2</ListGroup.Item>
-                <ListGroup.Item as="li">Преимущество 3</ListGroup.Item>
-              </ListGroup>
-              <Button disabled={subscription.name === 'Basic'} variant="secondary">
-                {subscription.name === 'Basic' ? 'Текущий' : 'Перейти'}
-              </Button>
-            </Card.Body>
-          </Card>
-          <Card className="text-center" style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>Премиум</Card.Title>
-              <ListGroup className="mb-2">
-                <ListGroup.Item as="li">Преимущество 1</ListGroup.Item>
-                <ListGroup.Item as="li">Преимущество 2</ListGroup.Item>
-                <ListGroup.Item as="li">Преимущество 3</ListGroup.Item>
-              </ListGroup>
-
-              <Button href="/payment" disabled={subscription.name !== 'Basic'} variant="warning">
-                {subscription.name !== 'Basic' ? 'Текущий' : 'Перейти'}
-              </Button>
-            </Card.Body>
-          </Card>
-        </div>
+        <div className="d-flex gap-3">{planCards}</div>
       </div>
     </>
   );
